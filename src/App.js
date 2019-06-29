f5ae075093860db7416c767b44511b035c2f577f
yiliang6 <yiliang6@illinois.edu>
2019-06-28 23:20:59 -0500

    Fix structure and style in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,67 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Countdown from './Countdown.js'
 import Particles from 'react-particles-js';
 import Logo from "./circle-cropped.png"
 import { LayersManager, Layer } from 'react-layers-manager'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 import './App.css';
 
-function App() {
-  const currentDate = new Date();
-  const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+class App extends Component {
+  render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
 
-  return (
-    <div className="mainbody">
-      <div className="home_content">
-        <img className="logo" src={Logo}/>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+    const { isOpen, onClose, message } = this.props;
+
+    return (
+      <div className="mainbody">
+        {/* <TransitionGroup component={null}>
+          {isOpen && (
+            <CSSTransition classNames="focus" timeout={1000}>
+              <div className="home-content">
+                <img className="logo" src={Logo}/>
+                <Countdown date={`${year}-12-24T00:00:00`} />
+              </div>
+            </CSSTransition>
+          )}
+        </TransitionGroup> */}
+        <div className="home_content">
+          <img className="logo" src={Logo}/>
+          <Countdown date={`${year}-12-24T00:00:00`} />
+        </div>
+        <Particles className="particles_top"
+        params={{
+          "particles": {
+              "number": {
+                  "value": 250,
+                  "density": {
+                      "enable": true,
+                      "value_area": 850
+                  }
+              },
+              "line_linked": {
+                  "enable": false,
+                  "opacity": 0.02
+              },
+              "move": {
+                  "direction": "up",
+                  "speed": 2
+              },
+              "size": {
+                  "value": 1
+              },
+              "opacity": {
+                  "anim": {
+                      "enable": true,
+                      "speed": 1,
+                      "opacity_min": 0.05
+                  }
+              }
+          },
+          "interactivity": {
+              "events": {
+                  "onclick": {
+                      "enable": true,
+                      "mode": "push"
+                  }
+              },
+              "modes": {
+                  "push": {
+                      "particles_nb": 1
+                  }
+              }
+          },
+          "retina_detect": true
+      }} />
       </div>
-      <Particles className="particles_top"
-      params={{
-        "particles": {
-            "number": {
-                "value": 250,
-                "density": {
-                    "enable": true,
-                    "value_area": 850
-                }
-            },
-            "line_linked": {
-                "enable": false,
-                "opacity": 0.02
-            },
-            "move": {
-                "direction": "up",
-                "speed": 2
-            },
-            "size": {
-                "value": 1
-            },
-            "opacity": {
-                "anim": {
-                    "enable": true,
-                    "speed": 1,
-                    "opacity_min": 0.05
-                }
-            }
-        },
-        "interactivity": {
-            "events": {
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                }
-            },
-            "modes": {
-                "push": {
-                    "particles_nb": 1
-                }
-            }
-        },
-        "retina_detect": true
-    }} />
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
