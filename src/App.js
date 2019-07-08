64e1c28ec3621721762f4d5a4261b8b203ffd80a
yiliang6 <yiliang6@illinois.edu>
2019-07-07 19:14:49 -0500

    Cleanup code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown.js'
 import Particles from 'react-particles-js';
-import Logo from "./circle-cropped.png"
-import { LayersManager, Layer } from 'react-layers-manager'
+import Logo from './circle-cropped.png'
+import { LayersManager, Layer } from 'react-layers-manager';
 
 import './App.css';
 
@@ -20,51 +20,50 @@ class App extends Component {
         <Particles className="particles-top"
         params={{
           "particles": {
-              "number": {
-                  "value": 250,
-                  "density": {
-                      "enable": true,
-                      "value_area": 850
-                  }
-              },
-              "line_linked": {
-                  "enable": false,
-                  "opacity": 0.02
-              },
-              "move": {
-                  "direction": "up",
-                  "speed": 1
-              },
-              "size": {
-                  "value": 1
-              },
-              "opacity": {
-                  "anim": {
-                      "enable": true,
-                      "speed": 1,
-                      "opacity_min": 0.05
-                  }
+            "number": {
+                "value": 300,
+                "density": {
+                "enable": true,
+                "value_area": 850
               }
+            },
+            "line_linked": {
+              "enable": false,
+              "opacity": 0.02
+            },
+            "move": {
+              "direction": "up",
+              "speed": 2
+            },
+            "size": {
+              "value": 1
+            },
+            "opacity": {
+              "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0.05
+              }
+            }
           },
           "interactivity": {
-              "events": {
-                  "onclick": {
-                      "enable": true,
-                      "mode": "push"
-                  }
-              },
-              "modes": {
-                  "push": {
-                      "particles_nb": 1
-                  }
+            "events": {
+              "onclick": {
+                "enable": true,
+                "mode": "push"
               }
+            },
+            "modes": {
+              "push": {
+                "particles_nb": 1
+              }
+            }
           },
           "retina_detect": true
-      }} />
+        }} />
       </div>
     );
   }
 }
-
 
 export default App;
