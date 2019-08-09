cd32d724bdd506e27d4c6863afabd319c7d7a3a3
Rishi Kalluri <rishidk22@gmail.com>
2019-08-08 21:21:58 -0500

    e

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       <div className="mainbody">
         <div className="home-content">
           <img className="logo" src={Logo}/>
-          <Countdown date={`${year}-12-24T00:00:00`} />
+          <Countdown date={`${year}-01-25T00:00:00`} />
           <Button class="sponsor_us_button">
           <a href="https://drive.google.com/file/d/1CCmoF_RuC-W2bVRUZGWsZsIwMsRSuZzo/view?usp=sharing">Sponsor Us</a>
           </Button>
