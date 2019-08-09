7fc2f3a0b1508f48a7466c69e71f364428f3cf6a
Rishi Kalluri <rishidk22@gmail.com>
2019-08-08 21:03:05 -0500

    coming soon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from "./circle-cropped.png"
 import { LayersManager, Layer } from 'react-layers-manager'
 import ParticleWrapper from './ParticleWrapper.js';
 import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+
 
 
 import './App.css';
@@ -19,7 +21,7 @@ class App extends Component {
           <img className="logo" src={Logo}/>
           <Countdown date={`${year}-12-24T00:00:00`} />
           <Button class="sponsor_us_button">
-            <span> <a href="./sponsor_us">Sponsor Us</a></span>
+          <a href="https://drive.google.com/file/d/1CCmoF_RuC-W2bVRUZGWsZsIwMsRSuZzo/view?usp=sharing">Sponsor Us</a>
           </Button>
         </div>
 
--- src/SponsorUs.js
+++ src/SponsorUs.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
-import ParticleWrapper from './ParticleWrapper';
+import { Document, Page, Outline } from 'react-pdf/dist/entry.webpack';
 
 class SponsorUs extends Component {
+
     render() {
-        return (
-            <div className="mainbody">
-                <h1>
-                    Register
-                </h1>
-                <ParticleWrapper />
-            </div>
-        );
+        return (<div>hello</div>
+        )
+
     }
 }
 
